2fe1db72781a7d4e76212351b865bd76a27a2015
Vũ Lơn Đinh <londv@dn.paracelsoft.com>
2022-11-26 21:43:57 +0700

    init

---
+++ src/components/repositoriesList.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+const Repositories: React.FC = () => {
+  const [term, setTerm] = useState<string>("");
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={term} onChange={(e) => setTerm(e.target.value)} />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
+export default Repositories;
--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,14 +1,14 @@
-import { ReportHandler } from 'web-vitals';
+import { ReportHandler } from "web-vitals";
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+const reportWebVitals = (onPerfEntry?: ReportHandler): any => {
+  if (onPerfEntry !== null && onPerfEntry instanceof Function) {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
-    });
+    }).then(() => console.log("this will success")).catch(() => console.log("this will fail"));
   }
 };
 
