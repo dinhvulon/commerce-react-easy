201e0ffdc64e33766c8948d05a84701cb4f7e9fc
dinhvulon <dinhvulon11@gmail.com>
2022-12-20 17:42:20 +0700

    d:

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,8 +1,9 @@
-import react, { useEffect } from "react";
+import react, { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import categoryApi from "../api/categoryApi";
 import { axiosClient } from "../api/axiosClient";
 const Home: React.FC = () => {
+  const [items, setItems] = useState<string[]>([]);
   useEffect(() => {
     const url = "/todos";
     const a = axiosClient.get(url, { params: "" }).then((response: any) => console.log(response));
@@ -10,6 +11,11 @@ const Home: React.FC = () => {
   return (
     <div>
       <Button variant="contained">Hello World 1</Button>
+      {
+        items?.map(e => {
+          return <div key={e}>{e}</div>;
+        })
+      }
       <button></button>
     </div>
   );
